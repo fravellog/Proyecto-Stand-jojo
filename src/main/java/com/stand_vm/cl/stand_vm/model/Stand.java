ac967e368c86f05f1fcb445eec0b52ed6d950f5c
fravellog <fr.avellog@duocuc.cl>
2025-05-17 21:41:37 -0400

    agregado de Data para solicitar datos

--- src/main/java/com/stand_vm/cl/stand_vm/model/Stand.java
+++ src/main/java/com/stand_vm/cl/stand_vm/model/Stand.java
@@ -2,12 +2,14 @@ package com.stand_vm.cl.stand_vm.model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "stand")
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
 
 public class Stand {
     @Id
