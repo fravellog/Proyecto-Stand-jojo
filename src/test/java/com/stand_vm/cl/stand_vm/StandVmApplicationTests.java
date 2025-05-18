efbfe9d4cfb55891319440f2d19e9168cdbbd551
fravellog <fr.avellog@duocuc.cl>
2025-05-17 20:37:57 -0400

    aplicacion de stand

---
+++ src/test/java/com/stand_vm/cl/stand_vm/StandVmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stand_vm.cl.stand_vm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StandVmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
