b3e0acc866bc28ac69c17bb2aa8f691c97f58dfe
fravellog <fr.avellog@duocuc.cl>
2025-05-17 20:36:08 -0400

    archivo de aplicacion de Stand

---
+++ src/main/java/com/stand_vm/cl/stand_vm/StandVmApplication.java
@@ -0,0 +1,13 @@
+package com.stand_vm.cl.stand_vm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StandVmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StandVmApplication.class, args);
+	}
+
+}
