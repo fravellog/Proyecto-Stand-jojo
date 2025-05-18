28743ac389085ead1a0860d51bdc33411f5ce069
fravellog <fr.avellog@duocuc.cl>
2025-05-17 20:37:43 -0400

    service de stand

---
+++ src/main/java/com/stand_vm/cl/stand_vm/service/StandService.java
@@ -0,0 +1,33 @@
+package com.stand_vm.cl.stand_vm.service;
+
+import com.stand_vm.cl.stand_vm.model.Stand;
+import com.stand_vm.cl.stand_vm.repository.StandRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class StandService {
+    @Autowired
+    private StandRepository standRepository;
+
+    public List<Stand> findAll() {
+        return standRepository.findAll();
+    }
+
+    public Stand findById(Long id) {
+        return standRepository.findById(id).orElse(null);
+    }
+
+    public Stand save(Stand stand) {
+        return standRepository.save(stand);
+    }
+
+    public void deleteById(Long id) {
+        standRepository.deleteById(id);
+    }
+
+}
