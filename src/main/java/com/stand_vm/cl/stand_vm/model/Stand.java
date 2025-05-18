277d96db0c67887871d663f2f4a6d006f44698b4
fravellog <fr.avellog@duocuc.cl>
2025-05-17 20:36:49 -0400

    modelo de stand

---
+++ src/main/java/com/stand_vm/cl/stand_vm/model/Stand.java
@@ -0,0 +1,25 @@
+package com.stand_vm.cl.stand_vm.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "stand")
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Stand {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 50)
+    private String nombre;
+
+    @Column(nullable = false, length = 50)
+    private String tipo;
+
+    @Column(nullable = false, length = 50)
+    private String habilidad;
+}
